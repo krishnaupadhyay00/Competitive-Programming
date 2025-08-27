72236a21cff7b7a235b80d9feec71c1158e653b9
Krishna Upadhyay <krishnaupadhyaymtr296@gmail.com>
2025-08-27 20:35:26 +0530

    Update last_digit_is_4.py

--- BCP(Python)/Sheet_2 Question/Sheet_1 Questions/last_digit_is_4.py
+++ BCP(Python)/Sheet_2 Question/Sheet_1 Questions/last_digit_is_4.py
@@ -1,9 +1,8 @@
-// Q3. Check if its  last digit is 4.
+# Q3. Check if its  last digit is 4.
 
 num = int(input("Enter a number: "))
-
-
 if num % 10 == 4:
     print("YES, the last digit is 4.")
 else:
     print("NO, the last digit is not 4.")
+
