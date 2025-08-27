3b202ca5b9e7bbf2c5fcde4bf1d0c471afd2d038
Krishna Upadhyay <krishnaupadhyaymtr296@gmail.com>
2025-08-27 20:33:33 +0530

    Update its_divisible_by_3.py

--- BCP(Python)/Sheet_2 Question/Sheet_1 Questions/its_divisible_by_3.py
+++ BCP(Python)/Sheet_2 Question/Sheet_1 Questions/its_divisible_by_3.py
@@ -1,9 +1,8 @@
-// Q5. Read a number check if its divisible by 3 or its last digit is 4.
+#Q5. Read a number check if its divisible by 3 or its last digit is 4.
 
 num = int(input("Enter a number: "))
-
-
 if num % 3 == 0 and num % 10 == 4:
     print("YES, the number is divisible by 3 and the last digit is 4.")
 else:
     print("NO, the number does not satisfy both conditions.")
+
