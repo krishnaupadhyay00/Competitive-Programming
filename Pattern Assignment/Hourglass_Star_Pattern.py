d6ba3dedae9e174e1e4b1e4d95a1f739bc6f27ad
Krishna Upadhyay <krishnaupadhyaymtr296@gmail.com>
2025-09-06 18:49:51 +0530

    Add files via upload

---
+++ Pattern Assignment/Hourglass_Star_Pattern.py
@@ -0,0 +1,19 @@
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+n=int(input())
+for i in range(n,0,-1):   # Top half
+    for j in range(i):
+        print("*" , end=" ")
+    print()
+for i in range(2,n+1):  # Bottom half
+    for j in range(i):
+        print("*" , end=" ")
+    print()
---
+++ Pattern Assignment/Symmetric Center-Aligned.py
@@ -0,0 +1,12 @@
+# * *
+# * *
+# *
+# * *
+# * *
+
+n=int(input())
+for i in range(n):
+    if i == 2:
+        print("*")
+    else:
+        print("* *")
